682b23a4efb2c26837a49de95324a75038b14a35
Stéphane Philippart <stephane.philippart@harmonie-mutuelle.fr>
2022-01-20 15:51:05 +0000

    :gear: SDK v2.x

--- src/main/java/fr/wilda/HelloWorldRunner.java
+++ src/main/java/fr/wilda/HelloWorldRunner.java
@@ -1,13 +1,13 @@
 package fr.wilda;
 
-import fr.wilda.controller.HelloWorldController;
+import fr.wilda.controller.HelloWorldReconciler;
 import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.config.runtime.DefaultConfigurationService;
 
 public class HelloWorldRunner {
     public static void main(String[] args) {
       Operator operator = new Operator(DefaultConfigurationService.instance());
-      operator.register(new HelloWorldController());
+      operator.register(new HelloWorldReconciler());
 
       System.out.println("🚀 Starting HelloWorld operator !!! 🚀");
       operator.start();
